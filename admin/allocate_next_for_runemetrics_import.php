e8ab19451a93630935fa04fa977ac9a2a938233e
Sam Dean <sam.dean@peters-research.com>
2019-10-02 13:04:53 +0100

    Revert "updated allocation logic"
    This reverts commit a30c975ec8072b24d4c4a3a1e623c30a927e4c0a.

--- admin/allocate_next_for_runemetrics_import.php
+++ admin/allocate_next_for_runemetrics_import.php
@@ -17,19 +17,14 @@ else {
             print_p("ERROR: ".mysqli_error($mysqli).". From sql query - \"$getUserToAllocateQuery\"", true);
         }
         else {
-            if ($getUserToAllocateResult->num_rows > 0) {
-                $userId = $getUserToAllocateResult->fetch_assoc()['id'];
-                $allocateUserQuery = "UPDATE users SET setup_state = 7, setup_state_change = NOW() WHERE id = $userId;";
-                $allocateUserResult = $mysqli->query($allocateUserQuery);
-                if (!$allocateUserResult) {
-                    print_p("ERROR: ".mysqli_error($mysqli).". From sql query - \"$allocateUserQuery\"", true);
-                }
-                else {
-                    print_p("$userId newly allocated");
-                }
+            $userId = $getUserToAllocateResult->fetch_assoc()['id'];
+            $allocateUserQuery = "UPDATE users SET setup_state = 7, setup_state_change = NOW() WHERE id = $userId;";
+            $allocateUserResult = $mysqli->query($allocateUserQuery);
+            if (!$allocateUserResult) {
+                print_p("ERROR: ".mysqli_error($mysqli).". From sql query - \"$allocateUserQuery\"", true);
             }
             else {
-                print_p("No one to allocate.");
+                print_p("$userId newly allocated");
             }
         }
     }
